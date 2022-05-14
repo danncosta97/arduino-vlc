a9acde0f6b227bc5c605b90598535f77ffc1045b
danncosta97 <danielbarbosa.0797@gmail.com>
2022-05-14 02:22:19 -0300

    [FINAL] - Estado do envio final
    Atualização de um arquivo com dado errado;
    Inserção da última versão do código registrada localmente;
    Inserção do documento final.

---
+++ Arduino/VLC_2021_12_14/ReceiverClean/ReceiverClean.ino
@@ -0,0 +1,235 @@
+// MEGA PORT COM 3 or COM 5 (USB DIR)
+//--------------------------
+// Port direct pin 2 -> PORTE[4] 000$0000
+// !!! ARDUINO IDE 1.18.6 !!! 
+// May fail on loading to board in newer IDE versions
+// Older IDE versions not tested
+
+// VERSAO COM INTERRUPCAO DO TIMER1 POR COMPARACAO E
+// COM INTERRUPCAO POR BORDA DE DESCIDA EM PIN 2
+
+#define IDLE_STATE 0 // Estado de aguardo do proximo Start Bit
+#define START_BIT_STATE 1 // Estado de envio do Start Bit
+#define DATA_BITS_STATE 2 // Estado de envio dos Bits do char
+#define STOP_BIT_STATE 3 // Estado de envio do Stop Bit
+
+#define STRING_SIZE 500
+
+#define FREQUENCY 4000
+#define OCR1A_FROM_FREQUENCY 16000000/FREQUENCY // NÃO MEXER
+#define OCR1A_FROM_FREQUENCY_HALF OCR1A_FROM_FREQUENCY/2    // NÃO MEXER
+
+#define TRANSMISSION_LIMIT 10   //Quantidade máxima de transmissões
+
+// Delay inicial para pegar o meio do bit (ponto ótimo)
+boolean initialdelay;
+
+// Aloca 1300 espaços para char (buffer de recebimento) para evitar multiplos Serial.print byte a byte
+// Valor máximo devido a memória do emissor
+char *stringReceivedBuffer = (char*) calloc(1301, sizeof(char));
+
+char charBits; // O char ASCII de fato (8 bits -> 1 byte)
+int bitReceived; // Valor do bit atual
+int bytesReceivedAmount; // Quantidade de bytes recebidos até o momento
+
+int state; // Estado atual do sistema
+boolean stateChangeLocker = 0; // Habilita execução do código de um estado
+
+int dataByteIndex; // indice do bit no byte de dado
+
+int finishedTransmissions; // Quantidade de transmissões realizadas
+
+void setup() {
+    // Habilita o Serial
+    Serial.begin(115200);
+    // Desliga o LED integrado
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // Fim de string
+    stringReceivedBuffer[STRING_SIZE] = '\0';
+
+    // DIGITAL PIN 7~2.TX.RX
+    // E7~E2.TX.RX
+    DDRE = DDRE & ~(B00010000); // inputs e outputs; pin 2 DIGITAL D2 (PORTA E4) input
+
+    charBits = 0;
+    bitReceived = 0;
+    bytesReceivedAmount = 0;
+
+    state = IDLE_STATE;
+
+    stateChangeLocker = 0;
+    dataByteIndex = 0;
+    finishedTransmissions = 0;
+    
+    // Desabilita interrupção global
+    cli();
+
+    // ##### Configura TIMER 1 #####
+    // Inicializa TIMER1
+    TCCR1A = 0;
+    TCCR1B = 0;
+    // Valor inicial do contador do TIMER1
+    TCNT1 = 0;
+
+    // Configura TIMER 1 para comparacao com CTC
+    // Modo CTC faz comparação direta com contagem ascendente [0->65536]
+    // ?? Mais simples de implementar o calculo (em relacao a OverFlow) ?? (creio que sim)
+    // ?? desepenho igual ao overflow ?? (a princípio sim)
+    TCCR1A |= B00000000;    // [7:6] -> Comparacao no Canal A: NORMAL [00];
+                            // [5:4] -> Comparacao no Canal B: NORMAL [00];
+                            // [3:2] -> RESERVADOS;
+                            // [1:0] -> Waveform Generation Mode:
+                            //          00 pois com TCCR1B [xxxx1xxx] forma CTC (CLEAR TIME ON COMPARE)
+                            //          reset o contador TCNT1 quando a flag de comparacao
+                            //          é acionada.
+                        
+    TCCR1B |= B00001001;    // [7]   -> Filtro de INPUT (?? delay de 4 ciclos ??);
+                            // [6]   -> Borda de captura do INPUT (0:descida; 1:subida);
+                            // [5]   -> RESERVADO;
+                            // [4:3] -> Waveform Generation Mode;
+                            // [2:0] -> Prescaler (setado em 256).
+                            
+    // Interrupção por timer será habilitada durante a interrupção externa (FALLING EDGE)
+    TIMSK1 |= B00000010;    // [7:6] -> RESERVADOS;
+                            // [5]   -> Habilita interrupcao por INPUT;
+                            // [4:3] -> RESERVADOS;
+                            // [2]   -> Habilita interrupcao por COMPARACAO no Canal B;
+                            // [1]   -> Habilita interrupcao por COMPARACAO no Canal A;
+                            // [0]   -> Habilita interrupcao por OVERFLOW do CONTADOR1;
+
+    // Valor a ser comparado no Canal A
+    // Com prescaler de 8
+    // 16000000 Hz/8 = 2000000 Hz -> Periodo de 0.5u
+    // 2000000 Hz/625 (contar até 625) = 3200 Hz -> Periodo de 312.5u
+    OCR1A = OCR1A_FROM_FREQUENCY;   //usar valor par
+    
+    //##### Configura interrupção externa (LOW, Any, Falling Edge, Rising Edge) #####
+    //[00] LOW LEVELS
+    //[01] ANY EDGE
+    //[10] FALLING EDGE
+    //[11] RISING EDGE
+    EICRA = B00000000;  //[7:6] INT3 - PIN18
+                        //[5:4] INT2 - PIN19
+                        //[3:2] INT1 - PIN20
+                        //[1:0] INT0 - PIN21
+    EICRB = B00000010;  //[7:6] INT7 - NA
+                        //[5:4] INT6 - NA
+                        //[3:2] INT5 - PIN3
+                        //[1:0] INT4 - PIN2 (D2) - Sendo utilizado no momento
+    // Ativa Ext Interrupt para INT7~0
+    EIMSK = B00010000;
+
+    // Habilita interrupção global
+    sei();
+}
+
+void loop() {
+    if(stateChangeLocker == 1){
+        
+        if(state == IDLE_STATE) {
+            stateChangeLocker = 0;
+        }
+        
+        if(state == START_BIT_STATE) {
+            // será sempre habilitada ao final da recepção de um pacote
+            cli();
+            // Habilita interrupções externas
+            EIMSK = B00000000;
+            sei();
+            
+            stateChangeLocker=0;
+        }
+    
+        if(state == DATA_BITS_STATE) {
+            bitReceived = PINE>>4 & B1;
+            charBits = charBits | (bitReceived << dataByteIndex);
+            dataByteIndex++;
+            stateChangeLocker=0;
+        }
+    
+        if(state == STOP_BIT_STATE) {
+            
+            if(PINE>>4 & B1 == 0) { // Não é um StopBit
+                stringReceivedBuffer[bytesReceivedAmount] = '?';
+            } else if ((charBits > 47 && charBits < 58) ||
+                       (charBits > 64 && charBits < 91) ||
+                       (charBits > 96 && charBits < 123)) { // Temos um alfanumerico
+                stringReceivedBuffer[bytesReceivedAmount] = charBits;
+            } else {    // Temos um falso positivo StopBit
+                stringReceivedBuffer[bytesReceivedAmount] = '?';
+            }
+            bytesReceivedAmount++;
+    
+            if(bytesReceivedAmount >= STRING_SIZE){
+                bytesReceivedAmount = 0;
+                Serial.println(stringReceivedBuffer);
+                finishedTransmissions++;
+            }
+                
+            charBits = 0;
+            stateChangeLocker=0;
+            
+            cli();
+            EIMSK = B00010000;
+            sei();
+
+            // Esse estado só ocorre entre transmissões completas (ou no início do sistema);
+            // Ja que em ISR(TIMER1_COMPA_vect) só entrará em IDLE_STATE
+            // quando nao houver um START_BIT (VALOR BAIXO) após STOP_BIT,
+            // pois o START_BIT forçará ISR(INT4_vect) antes de fechar o periodo para
+            // próxima chamada de ISR(TIMER1_COMPA_vect), onde há o reestabelecimento
+            // do timers para ler corretamente o próximo bit;
+            // Setar o IDLE_STATE aqui ao invés da próxima interrupção por tempo
+            // faz com que ocorra uma auto ajuste com da saída da mensagem (após algumas)
+            // tranmissões, caso o receptor seja iniciado no meio de uma transmissão.
+            state = IDLE_STATE;
+
+            // Ta bem complexo pensar o porque setar state em IDLE_STATE aqui força
+            // esse auto ajuste
+        }
+        
+        if(finishedTransmissions == TRANSMISSION_LIMIT) {
+            Serial.print("FINISHED");
+            // !!! SOMENTE PARA LIBERAR TODO O ULTIMO BUFFER NO SERIAL !!!
+            // NÃO IMPACTA PORQUE A ÚLTIMA TRNAMISSÃO JÁ FOI FINALIZADA
+            delay(1000);
+            exit(0);
+        }
+    }
+}
+
+// Interrupção externa (FALLING EDGE) em pin 2
+ISR(INT4_vect) {      
+
+    state = START_BIT_STATE;
+
+    // Metade do tempo definido em OCR1A - delay inicial ponto amostragem otimo
+    TCNT1 = OCR1A_FROM_FREQUENCY_HALF;
+    
+    // Com base, em TCNT1, para amostragem no ponto médio do bit
+    initialdelay = 1;
+    
+    stateChangeLocker = 0;
+}
+
+// Interrupção por COMPARACAO do TIMER/CONTADOR1 (CANAL A)
+ISR(TIMER1_COMPA_vect) {
+    if(state == IDLE_STATE) {
+        stateChangeLocker = 1;
+    } else if(state == START_BIT_STATE) {
+        stateChangeLocker = 1;
+        if(initialdelay == 1) {
+            initialdelay = 0;
+        } else if (initialdelay == 0) {
+            state = DATA_BITS_STATE;
+        }
+    }  else if(state == DATA_BITS_STATE) {
+        stateChangeLocker = 1;
+        if(dataByteIndex >= 8) {
+            dataByteIndex = 0;
+            state = STOP_BIT_STATE;
+        }
+    }
+}
---
+++ Arduino/VLC_2021_12_14/Transmitter/Transmitter.ino
@@ -0,0 +1,204 @@
+// NANO PORT COM 4 (USB ESQ)
+//--------------------------
+// Port direct pin 3 -> PORTD[2] 00000$00
+// !!! ARDUINO IDE 1.18.6 !!! 
+// May fail on loading to board in newer IDE versions
+// Older IDE versions not tested
+
+// VERSAO COM INTERRUPCAO DO TIMER1 POR COMPARACAO
+
+
+#define START_STATE 0 // Estado de inicializacao, tempo para iniciar o receptor
+#define START_BIT_STATE 1 // Estado de envio do Start Bit
+#define DATA_BITS_STATE 2 // Estado de envio dos Bits do char
+#define STOP_BIT_STATE 3 // Estado de envio do Stop Bit
+
+#define STRING_SIZE 500
+
+#define FREQUENCY 4000
+#define OCR1A_FROM_FREQUENCY 16000000/FREQUENCY // NÃO MEXER
+
+char string[STRING_SIZE+1] =  "jRAdmzYPhjhYE1RGURuDgMPBNuChB40209sHtyTPwE5IBLu1ppLjFdNNG6BVkh05463"\
+"G8fuUKmfYVP07N8qDJ1wn9mHFnX60CQIunQbLtDUBKl5ovpy8xPzmaa7C4roKiD5xt8"\
+"NSAYv4ERqu58R316TE3qzrMhimqGu2WkUmjqvjlpqOGsJNesr0JjioYnom1I6WWhCIi"\
+"iA5fGQUeJGT50JpFktpYx2GMYoYqrzYaYfa75wkqtMwdt5K5xPsuKsxup06LIcbfCfZ"\
+"Xcm9PB5FdkL67XFTg3kRJqPbtYxxS2qMBulx3hZIlqpJuo2aUc2dRgHRqAHsJZJd00A"\
+"DmxJJZ0cehE7fylPtdolQJFxKWDe06trKMHEyrfVX8N8nNPQWKeZnamH1t6yCCo7S1M"\
+"TBBPctjhwtGnFAHTvOf60gYIkbyHY5Eu2VYThJHl5k5BWV1TyKTUyXO2tjTxSfyd6Fs"\
+"yFxMNqpT1Z3VL2I1OdvnCZAhEAEZ4BZ";
+
+
+int stringLengthSent; //Qtd de char enviados
+
+int state; // Estado atual do sistema
+boolean stateChangeLocker; // Habilita execução do código de um estado
+
+int dataByteIndex; // indice do bit no byte de dado
+// @TODO - Não depender disso (Provavelmente dará trabalho)
+long sytemStartCounter = 0; // Valor alto entre envio de frases completas;
+                            // É necessário para que o buffer do receptor seja capaz de 
+                            // "empurrar" valor contido para fora e tenha o índice resetado
+                            // no receptor;
+                            // Deve ser uma valor maior que OCR1A e suficiente para o buffer,
+                            // do receptor printar no console Serial, pois assim o período em Idle
+                            // do sistema fará um auto ajuste (após algumas transmissões) na saída
+                            // da frase no buffer do receptor caso o sistema seja inciado no meio 
+                            // do envio de uma transmissão de dados.
+
+int finishedTransmissions;  // Quantidade de tranmissões realizadas
+                        
+void setup() {
+    // Habilita o Serial
+    Serial.begin(115200);
+    // Desliga o LED integrado
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+
+    DDRD = DDRD | B00000100; // inputs e outputs; pin 3 Output
+    PORTD = B00000100; // valor alto ou baixo; pin 3 Alto
+
+    stringLengthSent = 0;
+
+    state = START_STATE;
+    stateChangeLocker = 1;
+    dataByteIndex = 0;
+
+    // Intervalo entre transmissoes em segundos
+    // Com base em OCR1A
+    // Calcular segundosDesejados / (1 / (OCR1A / 16000000 / Prescaler) = x 
+    // 'x' é o valor a ser usado em sytemStartCounter
+    // AQUI CONFIGURA APENAS O INTERVALO ANTES DA PRIMEIRA TRANSMISSAO
+    // COLOCAR FREQUENCY * y para y segundos
+    sytemStartCounter = (long) FREQUENCY * 5;
+
+    // Desabilita interrupção global
+    cli();
+
+    // Inicializa TIMER1
+    TCCR1A = 0;
+    TCCR1B = 0;
+    // Valor inicial do contador do TIMER1
+    TCNT1 = 0;
+
+    // Configura TIMER 1 para comparacao com CTC
+    // Modo CTC faz comparação direta com contagem ascendente [0->65536]
+    // ?? Mais simples de implementar o calculo (em relacao a OverFlow) ?? (creio que sim)
+    // ?? desepenho igual ao overflow ?? (a princípio sim)
+    TCCR1A = B00000000;    // [7:6] -> Comparacao no Canal A: NORMAL [00];
+                            // [5:4] -> Comparacao no Canal B: NORMAL [00];
+                            // [3:2] -> RESERVADOS;
+                            // [1:0] -> Waveform Generation Mode:
+                            //          00 pois com TCCR1B [xxxx1xxx] forma CTC (CLEAR TIME ON COMPARE)
+                            //          reset o contador TCNT1 quando a flag de comparacao
+                            //          é acionada.
+                        
+    TCCR1B = B00001001;    // [7]   -> Filtro de INPUT (?? delay de 4 ciclos ??);
+                            // [6]   -> Borda de captura do INPUT (0:descida; 1:subida);
+                            // [5]   -> RESERVADO;
+                            // [4:3] -> Waveform Generation Mode;
+                            // [2:0] -> Prescaler (setado em 256).
+
+    TIMSK1 = 0b00000010;   // [7:6] -> RESERVADOS;
+                            // [5]   -> Habilita interrupcao por INPUT;
+                            // [4:3] -> RESERVADOS;
+                            // [2]   -> Habilita interrupcao por COMPARACAO no Canal B;
+                            // [1]   -> Habilita interrupcao por COMPARACAO no Canal A;
+                            // [0]   -> Habilita interrupcao por OVERFLOW do CONTADOR1;
+    
+    // Valor a ser comparado no Canal A
+    // Com prescaler de 8
+    // 16000000 Hz -> Periodo de 62.5ns
+    // EXEMPLO: 16000000 Hz/625 (contar até 625) = 3200 Hz -> Periodo de 312.5u
+    // FreqDesejada = (16000000/PRESCALER)/y, atribuir y à OCR1A
+    OCR1A = OCR1A_FROM_FREQUENCY;  //usar valor par
+
+    // Habilita interrupção global
+    sei();
+}
+
+void loop() {
+    if(stateChangeLocker==1){
+        
+        // Matém o LED aceso por 5s.
+        if(state == START_STATE && stateChangeLocker==1) {
+            PORTD = B00000100;
+            stateChangeLocker=0;
+            //Serial.println("*");
+        }
+    
+        // Faz o envio do Start Bit (nível baixo).
+        if(state == START_BIT_STATE && stateChangeLocker==1) {
+            PORTD = B00000000;
+            stateChangeLocker=0;
+            if(stringLengthSent == 0) {
+                Serial.println("SENDING...");
+            }
+            //Serial.print("$0$ ");
+        }
+    
+        // Faz o envio dos Bits do Char;
+        // Ao enviar 8 bits (baudDataBitIndex==8), vai para o Stop Bit
+        // e incrementa para o próximo caracter da string (stringLengthSent++).
+        if(state == DATA_BITS_STATE && stateChangeLocker==1) {
+            // Bits de dados (LSB para MSB)
+            if((string[stringLengthSent] & (0x01 << dataByteIndex)) != 0) {
+                PORTD = B00000100;
+                //Serial.print("1");
+            } else {
+                PORTD = B00000000;
+                //Serial.print("0");
+            }
+                dataByteIndex++;
+                
+            if(dataByteIndex >= 8) {
+                stringLengthSent++;
+            }
+            stateChangeLocker=0;
+        }
+    
+        // Faz o envio do Stop Bit (nível alto) e volta para o Start Bit;
+        // Se a qtd de char enviados for maior ou igual ao tamanho da frase
+        // Volta para estado de inicialização do sistema.
+        if(state == STOP_BIT_STATE && stateChangeLocker==1) {
+            PORTD = B00000100;
+            if(stringLengthSent == STRING_SIZE) {
+                Serial.println("SENT ");
+            }
+            //Serial.print(" #1#");
+            //Serial.println(string[stringLengthSent-1]);
+            stateChangeLocker=0;
+        }
+    }
+}
+
+// Interrupção por COMPARACAO do TIMER/CONTADOR1 (CANAL A)
+ISR(TIMER1_COMPA_vect) {
+    if(state == START_STATE) {
+        sytemStartCounter--;
+        if(sytemStartCounter <= 0L) {
+            state=START_BIT_STATE;
+            stateChangeLocker = 1;
+        }
+    } else if(state == START_BIT_STATE) {
+        state=DATA_BITS_STATE;
+        stateChangeLocker = 1;
+    } else if(state == DATA_BITS_STATE) {
+        if(dataByteIndex >= 8) {
+            dataByteIndex = 0;
+            state=STOP_BIT_STATE;
+        }
+        stateChangeLocker = 1;
+    } else if(state == STOP_BIT_STATE) {
+        if(stringLengthSent >= STRING_SIZE) {
+            stringLengthSent = 0;
+            state = START_STATE;
+            // DELAY ENTRE TRNAMISSÕES EM SEGUNDOS
+            // USER FREQUENCY / 2 para 0.5 Segundos
+            sytemStartCounter = (long) FREQUENCY * 0.5;
+        } else {
+            state=START_BIT_STATE;
+        }
+        stateChangeLocker = 1;
+    }
+}
